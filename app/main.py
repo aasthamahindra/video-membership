55185c93350e38afb918ba60e04c8e3174b59650
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2024-07-23 16:58:03 +0530

    start of application: main.py

---
+++ app/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from cassandra.cqlengine.management import sync_table
+from contextlib import asynccontextmanager
+from .users.models import User
+from . import db
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # triggers when FastAPI starts [@app.on_event deprecated]
+    db.get_session()
+    sync_table(User)
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def homepage():
+    return {"hello": "world"}
