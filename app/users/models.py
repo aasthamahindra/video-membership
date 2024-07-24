3991457e1b37a94ae5906b5846532c18d1924dc1
Aastha Mahindra <155626319+aasthamahindra@users.noreply.github.com>
2024-07-24 12:08:43 +0530

    email validation added while creating user

--- app/users/models.py
+++ app/users/models.py
@@ -2,6 +2,7 @@ import uuid
 from cassandra.cqlengine.models import Model
 from cassandra.cqlengine import columns
 from app.config import get_settings
+from . import validators
 
 settings = get_settings()
 
@@ -16,3 +17,19 @@ class User(Model):
 
     def __repr__(self):
         return f"User(email={self.email}, user_id={self.user_id})"
+    
+    @staticmethod
+    def create_user(email: str, password=None):
+        query = User.objects.filter(email=email)
+        if query.count() != 0:
+            raise Exception("User already exists!")
+        
+        # validate email
+        valid, msg, email = validators._validate_email(email=email)
+        if not valid:
+            raise Exception(f"Invalid email: {msg}")
+        
+        user = User(email=email)
+        user.password = password
+        user.save()
+        return user
